9cedf8ee394de9fd887f78136e207fd808051e18
Thamer Allahabi <thamer.a.allahabi@gmail.com>
2019-07-16 13:49:32 -0400

    Add files via upload

---
+++ Cycle_141.java
@@ -0,0 +1,21 @@
+public class Cycle_141 {
+
+
+}
+
+/*
+
+    public boolean hasCycle(ListNode head) {
+        Set<ListNode> set = new HashSet<>();
+        while(head != null){
+            if(set.contains(head)){
+                return true;
+            } else{
+                set.add(head);
+            }
+            head = head.next;
+        }
+        return false;
+    }
+*/
+
